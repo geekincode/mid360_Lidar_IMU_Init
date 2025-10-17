afe30d50181e46b9ffb7720ec380a031369ee076
geekincode <2949542342zj@gmail.com>
2025-10-17 16:28:22 +0800

    fix_docker

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -4,4 +4,4 @@
 
 echo "Building Docker image for mid360 Lidar IMU Init..."
 
-docker build -f ./docker/Dockerfile -t mid360_lidar_imu_init:v2.2 .
+docker build -f ./docker/Dockerfile -t mid360_lidar_imu_init:v3.0 .
--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,7 +2,7 @@
 
 # Run docker container for mid360 Lidar IMU Init project
 
-CONTAINER_NAME="mid360_lidar_imu_init_3"
+CONTAINER_NAME="mid360_lidar_imu_init"
 
 # Get host IP
 HOST_IP=$(hostname -I | awk '{print $1}')
@@ -12,6 +12,19 @@ export ROS_MASTER_URI=http://$HOST_IP:11311
 export ROS_IP=$HOST_IP
 
 # Allow X11 forwarding for GUI applications
+if [ $(docker container ls -a | grep -c $CONTAINER_NAME) -gt 0 ]; then
+    echo "Container $CONTAINER_NAME already exists."
+    
+    # Check if container is running
+    if [ $(docker container ls | grep -c $CONTAINER_NAME) -eq 0 ]; then
+        echo "Starting stopped container..."
+        docker start $CONTAINER_NAME
+    fi
+    
+    xhost +local:root
+    docker exec -it $CONTAINER_NAME /bin/bash
+    exit 0
+fi
 xhost +local:root
 
 # Get the dialout group ID from host
@@ -42,5 +55,5 @@ docker run --privileged -it \
     --env="ROS_MASTER_URI=$ROS_MASTER_URI" \
     --env="ROS_IP=$ROS_IP" \
     --env="KBUILD_NOPEDANTIC=1" \
-    mid360_lidar_imu_init:v2.2 \
+    mid360_lidar_imu_init:v3.0 \
     /bin/bash
